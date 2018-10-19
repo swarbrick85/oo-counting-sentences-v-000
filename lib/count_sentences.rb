030d356c88b14713ebd6d4f28cee2cdd6793972e
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-19 12:03:33 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    a = self.split("")
+    a.last == "."
   end
 
   def question?
-
+    a = self.split("")
+    a.last == "?"
   end
 
   def exclamation?
-
+    a = self.split("")
+    a.last == "!"
   end
 
   def count_sentences
-
+    x = []
+    a = self.split
+    a.each do |word|
+      if word.include? "."
+        x << word
+      elsif word.include? "?"
+        x << word 
+      elsif word.include? "!"
+        x << word
+      end
+    end 
+      x.size 
   end
+  
 end
